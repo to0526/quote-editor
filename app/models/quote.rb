38ad6541fbe4dbbf6b3408c8d313c053cacf270b
Taka <t.o.0526@gmail.com>
2024-11-25 21:39:28 +0900

    Broadcasting quote updates with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,5 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_prepend_to "quotes" }
 end

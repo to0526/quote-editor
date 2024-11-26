5b71e5708352129a4dc79b31f07312ee0156b33f
Taka <t.o.0526@gmail.com>
2024-11-26 21:39:05 +0900

    More syntactic sugar

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,5 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
